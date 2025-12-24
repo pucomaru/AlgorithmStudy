21e96d0dfb5d7fdbd0d42fb7b8560269bcbb8287
Hwannee00 <doormoo2@naver.com>
2025-12-24 21:57:11 +0900

    BOJ 25608 장난감 섞기 풀이 (누적합, 구간합)

--- java/src/dec_4/boj/장난감섞기_박재환.java
+++ java/src/dec_4/boj/장난감섞기_박재환.java
@@ -18,122 +18,88 @@ public class 장난감섞기_박재환 {
      */
     static StringTokenizer st;
     static int n, m;
-    static PriorityQueue<Pointer> leftList;
-    static PriorityQueue<Pointer> rightList;
-    static List<Pointer> mustAdd;
-    static int result;
+    static Sequence[] seqArr;
     static void init() throws IOException {
-        result = 0;
-        leftList = new PriorityQueue<>((a, b) -> {      // 왼쪽에 놓일 배열 [a < b]]
-            if(a.rightMax == b.rightMax) return Integer.compare(b.leftMax, a.leftMax);
-            return Integer.compare(b.rightMax, a.rightMax);
-        });
-        rightList = new PriorityQueue<>((a, b) -> {     // 오른쪽에 놓일 배열 [a > b]
-            if(a.leftMax == b.leftMax) return Integer.compare(b.rightMax, a.rightMax);
-            return Integer.compare(b.leftMax, a.leftMax);
-        });
-        mustAdd = new ArrayList<>();
         st = new StringTokenizer(br.readLine().trim());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n; i++) {
-            int[] arr = new int[m];
+        seqArr = new Sequence[n];
+        for(int i=0; i<n ;i++) {
             st = new StringTokenizer(br.readLine().trim());
+            int[] arr = new int[m];
             for(int j=0; j<m; j++) arr[j] = Integer.parseInt(st.nextToken());
-            Pointer p = new Pointer(arr);
-            if(p.leftMax == p.rightMax) mustAdd.add(p);
-            else if(p.leftMax < p.rightMax) {
-                leftList.offer(p);
-            } else {
-                rightList.offer(p);
-            }
+            seqArr[i] = new Sequence(arr);
         }
 
-//        for(Pointer p : leftList) System.out.printf("[%d, %d] ", p.leftMax, p.rightMax);
-//        for(Pointer p : mustAdd) System.out.printf("%d ", p.totalSum);
-//        for(Pointer p : rightList) System.out.printf("[%d, %d] ", p.leftMax, p.rightMax);
-
-        getLeftMax();
-        getRightMax();
-        getTotalSum();
+        int result = Math.max(getMaxSubArr(), getSequenceMaxSum());
         System.out.println(result);
     }
 
-    static void getLeftMax() {
-        if(leftList.isEmpty()) return;
-        Pointer firstPointer = leftList.poll();
-        int max = Math.max(firstPointer.totalSum, firstPointer.rightMax);
-        while(!leftList.isEmpty()) {
-            Pointer p = leftList.poll();
-            // 1. 이전 값 + 왼쪽
-            int case1 = Math.max(max, max+p.leftMax);
-            // 3. 이전값 + 전체
-            int case2 = Math.max(max, max+p.totalSum);
-            // 4. 다 버리고 max(왼쪽, 오른쪽)
-            int case3 = p.rightMax;
-            // 2. 이전값
-            max = Math.max(max, Math.max(Math.max(case1, case2), case3));
-        }
-        if(max >= 0) result += max;
+    static int getMaxSubArr() {
+        int max = 0;
+        for(Sequence s : seqArr) max = Math.max(max, s.subArr);
+        return max;
     }
 
-    static void getRightMax() {
-        if(rightList.isEmpty()) return;
-        Pointer firstPointer = rightList.poll();
-        int max = Math.max(firstPointer.leftMax, firstPointer.totalSum);
-        while(!rightList.isEmpty()) {
-            Pointer p = rightList.poll();
-            // 1. 이전 값 + 왼쪽
-            int case1 = Math.max(max, max+p.leftMax);
-            // 3. 이전값 + 전체
-            int case2 = Math.max(max, max+p.totalSum);
-            // 4. 다 버리고 max(왼쪽, 오른쪽)
-            int case3 = p.leftMax;
-            // 2. 이전값
-            max = Math.max(max, Math.max(Math.max(case1, case2), case3));
+    static int getSequenceMaxSum() {
+        int max = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i==j) continue;
+                int sum = seqArr[i].suffix + seqArr[j].prefix;
+                for(int z=0; z<n; z++) {
+                    if(z==i || z==j) continue;
+                    if(seqArr[z].total < 0) continue;
+                    sum += seqArr[z].total;
+                }
+                max = Math.max(max, sum);
+            }
         }
-        if(max >= 0) result += max;
+        return max;
     }
 
-    static void getTotalSum() {
-        for(Pointer p : mustAdd) {
-            if(p.totalSum >= 0) result += p.totalSum;
-        }
-    }
+    static class Sequence {
+        int prefix;
+        int suffix;
+        int total;
+        int subArr;
 
-    static class Pointer {
-        int leftMax;
-        int rightMax;
-        int totalSum;
-        int[] arr;
-
-        public Pointer(int[] arr) {
-            this.arr = arr;
-            getLRPointer();
+        public Sequence(int[] arr) {
+            this.setPrefix(arr);
+            this.setSuffix(arr);
+            this.setSubArr(arr);
         }
 
-        private void getLRPointer() {
-            int sum = arr[0];
+        private void setPrefix(int[] arr) {
+            int sum = 0;
             int max = sum;
-            for(int i=1; i<arr.length; i++) {
+            for(int i=0; i<m; i++) {
                 sum += arr[i];
                 max = Math.max(sum, max);
             }
-            this.leftMax = max;
+            this.prefix = max;
+            this.total = sum;
+        }
 
-            sum = arr[arr.length-1];
-            max = sum;
-            for(int i=arr.length-2; i>-1; i--) {
+        private void setSuffix(int[] arr) {
+            int sum = 0;
+            int max = sum;
+            for(int i=m-1; i>-1; i--) {
                 sum += arr[i];
                 max = Math.max(sum, max);
             }
-            this.rightMax = max;
+            this.suffix = max;
+        }
 
-            totalSum = 0;
-            for(int i : arr) {
-                totalSum+=i;
+        private void setSubArr(int[] arr) {
+            int localMax = 0;
+            int globalMax = localMax;
+            for(int i=0; i<m; i++) {
+                localMax = Math.max(0, arr[i] + localMax);
+                globalMax = Math.max(globalMax, localMax);
             }
+            this.subArr = globalMax;
         }
     }
 }
