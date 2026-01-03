50bae71a6454ac044f4ad2d4cd4c7a86b8364300
Hwannee00 <doormoo2@naver.com>
2026-01-03 11:30:53 +0900

    BOJ 16946 벽 부수고 이동하기 4 풀이중 - 박재환[JAVA]

---
+++ java/src/jan/week1/boj/벽부수고이동하기4_박재환.java
@@ -0,0 +1,23 @@
+package jan.week1.boj;
+
+import java.util.*;
+import java.io.*;
+
+public class 벽부수고이동하기4_박재환 {
+    static BufferedReader br;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        init();
+        br.close();
+    }
+    static StringTokenizer st;
+    static int n, m;
+    static int[][] map;
+    static void init() throws IOException {
+        st = new StringTokenizer(br.readLine().trim());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+
+    }
+}
