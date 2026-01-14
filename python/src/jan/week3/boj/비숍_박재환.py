ff391585f3896256db37f730a58cd3351e3eb19a
Park Jae Hwan <doormoo2@naver.com>
2026-01-14 18:28:58 +0900

    BOJ 1799 비숍 풀이(백트래킹) - 박재환[PYTHON]

--- python/src/jan/week3/boj/비숍_박재환.py
+++ python/src/jan/week3/boj/비숍_박재환.py
@@ -5,29 +5,11 @@ def solution():
 
     n = int(input())
     board = [list(map(int, input().split())) for _ in range(n)]
-    is_used = [[0]*n for _ in range(n)]
+    diagoal_left = [0]*(2*n)        # 대각선 ( / )
+    diagoal_right = [0]*(2*n)       # 대각선 ( \ )
 
     white_bishop = 0
     black_bishop = 0
-
-    def is_not_board(x, y):
-        return x < 0 or y < 0 or x >= n or y >= n
-
-    dx = [1,1,-1,-1]
-    dy = [1,-1,1,-1]
-    def check_uncheck(x, y, check):
-        for dir in range(4):
-            cur_x = x
-            cur_y = y
-
-            while not is_not_board(cur_x, cur_y):
-                if check:
-                    is_used[cur_x][cur_y] += 1
-                else:
-                    is_used[cur_x][cur_y] = max(0, is_used[cur_x][cur_y]-1)
-
-                cur_x += dx[dir]
-                cur_y += dy[dir]
 
     def put_bishop(idx, bishop_count, color):
         nonlocal white_bishop, black_bishop
@@ -42,15 +24,17 @@ def solution():
 
             if board[x][y] == 0:
                 continue
-            if is_used[x][y] > 0:
+            if diagoal_left[(x+y)] > 0 or diagoal_right[(n-1) + (x-y)] > 0:
                 continue
             if (x+y)%2 == color:
                 continue
                 
             # 비숍을 놓을 수 있음
-            check_uncheck(x, y, True)
-            put_bishop(idx+1, bishop_count+1, color)
-            check_uncheck(x, y, False)
+            diagoal_left[(x + y)]+=1
+            diagoal_right[(n - 1) + (x - y)] += 1
+            put_bishop(i+1, bishop_count+1, color)
+            diagoal_left[(x + y)] -= 1
+            diagoal_right[(n - 1) + (x - y)] -= 1
 
     put_bishop(0,0,0)
     put_bishop(0,0,1)
