32cd4a83377e1b1ac4fbf4f4a02470bc19095b0b
Park Jae Hwan <doormoo2@naver.com>
2026-01-16 12:55:27 +0900

    BOJ 33679 세기의 대결 풀이(LIS) - 박재환[JAVA]

--- java/src/jan/week3/boj/세기의대결_박재환.java
+++ java/src/jan/week3/boj/세기의대결_박재환.java
@@ -36,21 +36,37 @@ public class 세기의대결_박재환 {
      */
     static final String SUFFIX = "Win!";
     static String findWinner() {
-        int yj = getLisLength(0);
-        int hg = getLisLength(1);
+        int yj = getMaxLisLength(0);
+        int hg = getMaxLisLength(1);
 
         if(yj == hg) return "Both "+SUFFIX;
 
-        return (yj > hg ? "YG " : "HG ") + SUFFIX;
+        return (yj > hg ? "YJ " : "HG ") + SUFFIX;
     }
 
-    static int getLisLength(int idx) {
+    static int getMaxLisLength(int idx) {
+        int maxLisLen = 1;
+
+        for(int turn=0; turn<n; turn++) {       // 총알을 돌려가며 각 위치에서 최대 값을 구한다.
+            maxLisLen = Math.max(maxLisLen, getLisLength(bullets[idx]));
+            // 배열을 회전시킨다. -> 한 칸씩 민다.
+            int[] tempBullet = new int[n];
+            tempBullet[0] = bullets[idx][n-1];
+            for(int i=1; i<n; i++) {
+                tempBullet[i] = bullets[idx][i-1];
+            }
+            bullets[idx] = tempBullet;
+        }
+        return maxLisLen;
+    }
+
+    static int getLisLength(int[] bullet) {
         int[] lisArr = new int[n];
 
         for(int i=0; i<n; i++) {
             lisArr[i] = 1;      // 자기 자신을 포함하는 길이 ( 1 )
             for(int j=0; j<i; j++) {
-                if(bullets[idx][i] > bullets[idx][j]) {     // 증가하는 부분수열이라면
+                if(bullet[i] > bullet[j]) {     // 증가하는 부분수열이라면
                     lisArr[i] = Math.max(lisArr[i], lisArr[j]+1);       // 이전 위치들의 최장 증가 부분 수열의 길이와 비교
                 }
             }
