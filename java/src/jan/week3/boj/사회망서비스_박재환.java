a488623aea31a3b770ed29a2036dff5e396a026b
Hwannee00 <doormoo2@naver.com>
2026-01-17 19:32:14 +0900

    BOJ 2533 SNS 풀이(DFS, 바텀업) - 박재환[JAVA]

--- java/src/jan/week3/boj/사회망서비스_박재환.java
+++ java/src/jan/week3/boj/사회망서비스_박재환.java
@@ -23,25 +23,50 @@ public class 사회망서비스_박재환 {
      */
     static StringTokenizer st;
     static List<Integer>[] connections;
-    static int[] inEdges;
+    static boolean[] visited;
+    static int minAdCount;
     static void init() throws IOException {
+        minAdCount = 0;
+
         int connectionCount = Integer.parseInt(br.readLine().trim());
         connections = new List[connectionCount+1];
-        inEdges = new int[connectionCount+1];
+        visited = new boolean[connectionCount+1];
         for(int i=0; i<connectionCount+1; i++) connections[i] = new ArrayList<>();
 
-        while(connectionCount-- > 0) {
+        while(connectionCount-- > 1) {      // n-1 개의 간선 정보가 주어짐
             st = new StringTokenizer(br.readLine().trim());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-
-            inEdges[from]++;
+            
             connections[from].add(to);
+            connections[to].add(from);
         }
 
         /**
          * 부모 노드가 AD 가 아니라면, 자식 노드는 항상 AD 여야함
          * 부모 노드가 AD 라면, 자식 노드는 AD 가 필수가 아님
          */
+        isAd(1);
+
+        System.out.println(minAdCount);
+    }
+
+    static boolean isAd(int node) {        // 현재 노드가 AD 이어야 하는지 판단
+        visited[node] = true;
+
+        boolean ad = false;
+        // 현재 노드와 연결된 다른 노드들을 확인
+        for(int connection : connections[node]) {
+            if(visited[connection]) continue;       // 이미 확인한 노드
+
+            // 리프노드부터 탐색할 수 있도록 바텀업으로 구성
+            if(!isAd(connection)) {     // 자식노드가 AD 가 아니라면, 부모 노드는 반드시 AD
+                ad = true;
+            }
+        }
+
+        if(ad) minAdCount++;
+
+        return ad;
     }
 }
