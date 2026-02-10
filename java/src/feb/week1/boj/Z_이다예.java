849efd2d442ecbc1bbc9a682a3efa2815f9b1476
다예 <myannnng@gmail.com>
2026-02-10 20:10:40 +0900

    Solving

---
+++ java/src/feb/week1/boj/Z_이다예.java
@@ -0,0 +1,21 @@
+package feb.week1.boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Z_이다예 {
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N,r,c;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+
+    }
+}
