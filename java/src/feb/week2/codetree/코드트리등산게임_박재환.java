9ca983cea8933a4729d78c6bec51d6f34d13281e
Hwannee00 <doormoo2@naver.com>
2026-02-13 00:07:21 +0900

    CODETREE 코드트리 등산 게임 풀이(LIS + Binary Search + 자료구조) - 박재환

--- java/src/feb/week2/codetree/코드트리등산게임_박재환.java
+++ java/src/feb/week2/codetree/코드트리등산게임_박재환.java
@@ -5,10 +5,13 @@ import java.io.*;
 
 public class 코드트리등산게임_박재환 {
     static BufferedReader br;
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
         init();
         br.close();
+        System.out.println(sb);
     }
     /**
      *  지도에는 산들이 연속으로 나열되어 있다.
@@ -29,148 +32,95 @@ public class 코드트리등산게임_박재환 {
      *  케이블 카를 탈 수 있는 산에 도착했다면 케이블 카를 이용한다. -> 이용에 성공하면 1,000,000점을 얻는다.
      *  최종적으로 위치한 산의 높이만큼 점수를 얻는다.
      */
+    static final int SCORE = 1_000_000;
+
     static StringTokenizer st;
     static void init() throws IOException {
         int q = Integer.parseInt(br.readLine().trim());
+
         while(q-- > 0) {
             st = new StringTokenizer(br.readLine().trim());
-            int command = Integer.parseInt(st.nextToken());
+            int cmd = Integer.parseInt(st.nextToken());
 
-            switch(command) {
+            switch(cmd) {
                 case 100:{
-                    /**
-                     *  n, h[1],...h[n]
-                     *  n 개의 산으로 이루어져 있으며, 왼쪽부터 높이가 주어진다.
-                     */
-                    makeMountain();
+                    initMountain();
                     break;
                 }
                 case 200:{
-                    /**
-                     * 기존 산의 오른쪽에 h 높이를 갖는 산을 추가한다.
-                     */
+                    addMountain();
                     break;
                 }
                 case 300:{
-                    /**
-                     * 기존 산들 중, 가장 오른쪽에 위치한 산을 제거한다.
-                     */
+                    removeMountain();
                     break;
                 }
                 case 400:{
-                    /**
-                     * 케이블카를 이용할 수 있는 산의 정보 m 이 주어진다.
-                     * 등산 시뮬레이션 중 얻을 수 있는 최대 점수를 출력한다.
-                     */
+                    sb.append(getScore()).append('\n');
                     break;
                 }
             }
         }
-
     }
-    static int n;
-    static int[] mountains;     // 산의 높이
-    static void makeMountain() {
-        n = Integer.parseInt(st.nextToken());
-        mountains = new int[n];
-        for(int i=0; i<n; i++) mountains[i] = Integer.parseInt(st.nextToken());
+    static int mountainCount;
+    static List<ArrayDeque<Integer>> mountains;
+    static List<Integer> stack;
+    static void initMountain() {
+        mountains = new ArrayList<>();
+        stack = new ArrayList<>();
 
-    }
-    static int[] getLis() {
-        int[] compress = compress();
-        int maxValue = 0;
-        for(int i : compress) maxValue = Math.max(maxValue, i);
-
-        SegmentTree segmentTree = new SegmentTree(maxValue);
-        int[] dp = new int[n];
-
-        for(int i=0; i<n; i++) {
-            int height = compress[i];
-
-            int best = 0;
-            if(height > 1) {        // 나보다 작은 최대 DP 찾기
-                best = segmentTree.query(1, 1, maxValue, 1, height-1);
-            }
-
-            dp[i] = best+1;
-            segmentTree.update(1, 1, maxValue, height, height, dp[i]);
-        }
-        return dp;
-    }
-    static int[] getReverseLis() {
-        int[] compress = compress();
-        int maxValue = 0;
-        for(int i : compress) maxValue = Math.max(maxValue, i);
-
-        SegmentTree segmentTree = new SegmentTree(maxValue);
-        int[] dp = new int[n];
-
-        for(int i=n-1; i>-1; i--) {
-            int height = compress[i];
-
-            int best = 0;
-            if(height < maxValue) {     // 나보다 큰 위치의 DP 찾기
-                best = segmentTree.query(1, 1, maxValue, height+1, maxValue);
-            }
-
-            dp[i] = best+1;
-            segmentTree.update(1, 1, maxValue, height, height, dp[i]);
-        }
-        return dp;
-    }
-    static int[] compress() {
-        /**
-         * 좌표 압축
-         */
-        int[] temp = new int[n];
-        for(int i=0; i<n; i++) temp[i] = mountains[i];
-        Arrays.sort(temp);
-
-        Map<Integer, Integer> map = new HashMap<>();
-        int id = 1;
-
-        for(int i : temp) {
-            if(!map.containsKey(i)) {
-                map.put(i, id++);
-            }
-        }
-        int[] result = new int[n];
-        for(int i=0; i<n; i++) result[i] = map.get(mountains[i]);
-        return result;
-    }
-    static class SegmentTree {
-        /**
-         * tree[height]
-         * : 현재 height 에서의 LIS 최대값
-         */
-        int size;
-        int[] tree;
-
-        SegmentTree(int size) {
-            this.size = size;
-            this.tree = new int[4*size];
-        }
-        int query(int id, int l, int r, int s, int e) {
-            if(r < s || l > e) return 0;      // 범위에 완전하게 포함되지 않음
-            if(l >= s && r <= e) {          // 범위에 완전하게 포함됨
-                return tree[id];
-            }
-            int mid = l + (r-l)/2;
-            return Math.max(query(2*id, l ,mid, s, e), query(2*id+1, mid+1, r, s, e));
-        }
-        void update(int id, int l, int r, int s, int e, int targetValue) {
+        mountainCount = Integer.parseInt(st.nextToken());
+        for(int i=0; i<mountainCount; i++) {
             /**
-             * 현재 높이에서 최대 값(LIS) 구하기
+             * LIS 생성
+             * - List<ArrayDeque<Integer>>
+             *     => List.size() : LIS 길이
+             *     => ArrayDeque  : 같은 LIS 길이의 원소들 (마지막 값이 가장 작은 값)
              */
-            if(r < s || l > e) return;      // 범위에 완전하게 포함되지 않음
-            if(l >= s && r <= e) {          // 범위에 완전하게 포함됨
-                tree[id] = Math.max(tree[id], targetValue);
-                return;
-            }
-            int mid = l + (r-l)/2;
-            update(2*id, l, mid, s, e, targetValue);
-            update(2*id+1, mid+1, r, s, e, targetValue);
-            tree[id] = Math.max(tree[2*id], tree[2*id+1]);
+            int height = Integer.parseInt(st.nextToken());
+            int insertId = mountains.isEmpty() ? 0 : findInsertId(height);
+            stack.add(insertId);
+            if(insertId == mountains.size()) mountains.add(new ArrayDeque<>());         // 새로운 LIS 갱신
+            mountains.get(insertId).offerLast(height);
         }
+    }
+    static void addMountain() {
+        int height = Integer.parseInt(st.nextToken());
+        int insertId = mountains.isEmpty() ? 0 : findInsertId(height);
+        stack.add(insertId);
+        if(insertId == mountains.size()) mountains.add(new ArrayDeque<>());         // 새로운 LIS 갱신
+        mountains.get(insertId).offerLast(height);
+    }
+    static void removeMountain() {
+        /**
+         * lastLis : 마지막에 위치한 Height 의 LIS
+         */
+        int lastId = stack.size()-1;
+        int lastLis = stack.get(lastId);
+        mountains.get(lastLis).pollLast();
+        if(mountains.get(lastLis).isEmpty()) mountains.remove(lastLis);     // 더 이상 해당 Level 에 원소가 없다면 -> 전체 LIS 가 1 감소
+        stack.remove(lastId);
+    }
+    static long getScore() {
+        int cableMountain = Integer.parseInt(st.nextToken())-1;
+        long score = (stack.get(cableMountain) + mountains.size()) * SCORE;
+        score += mountains.get(mountains.size()-1).peekFirst();
+        return score;
+    }
+    static int findInsertId(int target) {
+        /**
+         * Lower Bound
+         * (target <= VALUE)
+         */
+        int l=0, r=mountains.size();
+
+        while(l < r) {
+            int mid = l + (r-l)/2;
+            int minValue = mountains.get(mid).peekLast();
+
+            if(target <= minValue) r = mid;      // 정답에 포함될 수 있으므로 mid 포함
+            else l = mid+1;
+        }
+        return l;
     }
 }
